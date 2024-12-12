6bb6803d9a9b59396f0aad88a6179eb8942b0953
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2024-12-13 10:24:49 +1100

    Added socials buttons with incomplete hrefs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import React, { useEffect, useState } from 'react'
 import Logo from 'public/images/logo-transparent-3.png'
 import Image from 'next/image'
@@ -6,15 +6,22 @@ import Image from 'next/image'
 import { PiPhoneCallThin } from 'react-icons/pi'
 import HeaderButtons from './HeaderButtons'
 
+import { FaGoogle } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { FaFacebookF } from 'react-icons/fa'
+import Link from 'next/link'
+
 export const Header = () => {
   const [bgState, setBgState] = useState<boolean>(false)
 
   useEffect(() => {
     const transitionValue = 100
     const scrollValue = window.scrollY
-    if (scrollValue > transitionValue){
+    if (scrollValue > transitionValue) {
       setBgState(true)
-    } else { setBgState(false)}
+    } else {
+      setBgState(false)
+    }
   }, [])
 
   return (
@@ -30,9 +37,20 @@ export const Header = () => {
         <HeaderButtons title="Projects" page="projects" />
         <HeaderButtons title="Contact" page="contact" />
       </div>
-      <div className="flex flex-row h-12 text-sm tracking-widest items-center">
+      {/* <div className="flex flex-row h-12 text-sm tracking-widest items-center">
         <PiPhoneCallThin size={30} color={'#fff'} />
         <h2 className="text-headerText align-middle font-robotoSerif">+61 400 000 000</h2>
+      </div> */}
+      <div className="flex flex-row gap-5 h-12 items-center">
+        <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank">
+          <FaGoogle size={30} color={'#fff'} />
+        </Link>
+        <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank">
+          <FaInstagram size={30} color={'#fff'} />
+        </Link>
+        <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank">
+          <FaFacebookF size={30} color={'#fff'} />
+        </Link>
       </div>
     </div>
   )
