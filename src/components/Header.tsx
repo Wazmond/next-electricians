3f69c15a14440c6fc585f89c00ef500254e285d7
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-24 03:20:49 +1100

    Styling changes, and adding nav menu for smaller devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,13 @@ import Logo from 'public/images/logo-transparent-3.png'
 import Image from 'next/image'
 
 import { PiPhoneCallThin } from 'react-icons/pi'
-import HeaderButtons from './HeaderButtons'
+import HeaderButtons from './header/HeaderButtons'
 
 import { FaGoogle } from 'react-icons/fa'
 import { FaInstagram } from 'react-icons/fa'
 import { FaFacebookF } from 'react-icons/fa'
 import Link from 'next/link'
+import NavButtons from './header/SmNav'
 
 export const Header = () => {
   const [bgState, setBgState] = useState<boolean>(false)
@@ -26,61 +27,75 @@ export const Header = () => {
   }, [])
 
   return (
-    <div className={`fixed z-10 flex flex-row h-16 w-full justify-between md:justify-evenly px-5`}>
-      <Image src={Logo} alt="logo" height={64} />
+    <>
       <div
-        // className=" md:flex transition-all duration-300 ease-in-out w-0 overflow-hidden md:w-[386.8px] h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
-        className="hidden md:flex h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
-        style={{ color: 'var(--header-text-color)' }}
+        className={`absolute z-[9] flex flex-col transform ${menuState ? 'scale-y-100' : 'scale-y-0'} origin-top overflow-hidden w-full bg-white transition-transform duration-150 ease-in-out`}
       >
-        <HeaderButtons title="Home" page="" />
-        <HeaderButtons title="About" page="about" />
-        <HeaderButtons title="Services" page="services" />
-        <HeaderButtons title="Projects" page="projects" />
-        <HeaderButtons title="Contact" page="contact" />
+        <span className='h-20 border-b-2 border-mono-500'/>
+        <div>
+          <NavButtons redir="" title="Home" setMenuState={setMenuState} />
+          <NavButtons redir="about" title="About" setMenuState={setMenuState} />
+          <NavButtons redir="services" title="Services" setMenuState={setMenuState} />
+          <NavButtons redir="projects" title="Projects" setMenuState={setMenuState} />
+          <NavButtons redir="contact" title="Contact" setMenuState={setMenuState} />
+        </div>
       </div>
-      {/* <div className="flex flex-row h-12 text-sm tracking-widest items-center">
+      <div
+        className={`fixed z-10 flex flex-row h-16 w-full justify-between md:justify-evenly px-5`}
+      >
+        <Image src={Logo} alt="logo" height={64} />
+        <div
+          // className=" md:flex transition-all duration-300 ease-in-out w-0 overflow-hidden md:w-[386.8px] h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
+          className="hidden md:flex h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
+          style={{ color: 'var(--header-text-color)' }}
+        >
+          <HeaderButtons title="Home" page="" />
+          <HeaderButtons title="About" page="about" />
+          <HeaderButtons title="Services" page="services" />
+          <HeaderButtons title="Projects" page="projects" />
+          <HeaderButtons title="Contact" page="contact" />
+        </div>
+        {/* <div className="flex flex-row h-12 text-sm tracking-widest items-center">
         <PiPhoneCallThin size={30} color={'#fff'} />
         <h2 className="text-headerText align-middle font-robotoSerif">+61 400 000 000</h2>
       </div> */}
 
-      <div className="hidden md:flex flex-row gap-5 h-12 items-center">
-        <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank" aria-label="Google">
-          <FaGoogle size={25} color={'#fff'} />
-        </Link>
-        <Link
-          href={'https://www.instagram.com/nextelectricians/'}
-          target="_blank"
-          aria-label="Instagram"
-        >
-          <FaInstagram size={25} color={'#fff'} />
-        </Link>
-        <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank" aria-label="Facebook">
-          <FaFacebookF size={25} color={'#fff'} />
-        </Link>
-      </div>
+        <div className="hidden md:flex flex-row gap-5 h-12 items-center">
+          <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank" aria-label="Google">
+            <FaGoogle size={25} color={'#fff'} />
+          </Link>
+          <Link
+            href={'https://www.instagram.com/nextelectricians/'}
+            target="_blank"
+            aria-label="Instagram"
+          >
+            <FaInstagram size={25} color={'#fff'} />
+          </Link>
+          <Link href={'https://g.co/kgs/DXmE18Y'} target="_blank" aria-label="Facebook">
+            <FaFacebookF size={25} color={'#fff'} />
+          </Link>
+        </div>
 
-      <div
-        className="md:hidden flex flex-col h-12 w-12 items-center justify-evenly hover:cursor-pointer"
-        aria-label="Menu Button"
-      >
-        <button
-          className="flex flex-col justify-between h-6 w-8"
-          onClick={() => {
-            setMenuState(!menuState), console.log(menuState)
-          }}
+        <div
+          className="md:hidden flex flex-col h-12 w-12 items-center justify-evenly hover:cursor-pointer"
+          aria-label="Menu Button"
         >
-          <span
-            className={`bg-white h-0.5 w-8 transition-all ${menuState ? 'rotate-45 translate-y-2.5' : ''}`}
-          />
-          <span
-            className={`bg-white h-0.5 w-8 transition-opacity ${menuState ? 'opacity-0' : 'opacity-100'}`}
-          />
-          <span
-            className={`bg-white h-0.5 w-8 transition-all ${menuState ? '-rotate-45 -translate-y-2.5' : ''}`}
-          />
-        </button>
+          <button
+            className="flex flex-col justify-between h-6 w-8"
+            onClick={() => setMenuState(!menuState)}
+          >
+            <span
+              className={`${menuState ? 'bg-black' : 'bg-white'} h-0.5 w-8 transition-all ${menuState ? 'rotate-45 translate-y-2.5' : ''}`}
+            />
+            <span
+              className={`bg-white h-0.5 w-8 transition-opacity ${menuState ? 'opacity-0' : 'opacity-100'}`}
+            />
+            <span
+              className={`${menuState ? 'bg-black' : 'bg-white'} h-0.5 w-8 transition-all ${menuState ? '-rotate-45 -translate-y-2.5' : ''}`}
+            />
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
