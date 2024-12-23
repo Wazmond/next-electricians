d7b808bc96e2443617d7866a6687632d83eb7470
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-24 03:19:54 +1100

    Relocated file to Header Folder

---
+++ src/components/header/HeaderButtons.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import React from 'react'
+
+interface Props {
+  title: string
+  page: string
+}
+
+const HeaderButtons: React.FC<Props> = ({ title, page }) => {
+  return (
+    <Link href={`/${page}`}>
+      <div
+        id="button"
+        className="flex flex-col h-full justify-center group hover:cursor-pointer"
+      >
+        <h2 className="text-sm group-hover:text-neutral-400 text-white">{title}</h2>
+        <span
+          className={`h-px w-full scale-x-0 transform bg-white transition-transform duration-200 ease-in-out origin-center group-hover:scale-x-100`}
+        />
+      </div>
+    </Link>
+  )
+}
+
+export default HeaderButtons
