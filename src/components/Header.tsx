58868617a7ca837990d4946f08fb1a9e98b66563
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-25 16:46:29 +1100

    Cleaning up code and added close on click outside of menu div.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,11 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Logo from 'public/images/logo-transparent-3.png'
 import Image from 'next/image'
 
 import { PiPhoneCallThin } from 'react-icons/pi'
 import HeaderButtons from './header/HeaderButtons'
 
-import { FaGoogle } from 'react-icons/fa'
-import { FaInstagram } from 'react-icons/fa'
-import { FaFacebookF } from 'react-icons/fa'
 import Link from 'next/link'
 import NavButtons from './header/SmNav'
 import Socials from './header/socials'
@@ -16,6 +13,19 @@ import Socials from './header/socials'
 export const Header = () => {
   const [bgState, setBgState] = useState<number>(0.0)
   const [menuState, setMenuState] = useState<boolean>(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handleClose = (e: MouseEvent) => {
+      if (!menuRef.current?.contains(e.target as Node)) {
+        setMenuState(false)
+      }
+    }
+    window.addEventListener('mousedown', handleClose)
+    return () => {
+      window.removeEventListener('mousedown', handleClose)
+    }
+  }, [])
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,7 +34,6 @@ export const Header = () => {
       scrollValue < transitionValue ? setBgState(scrollValue / transitionValue) : setBgState(1)
     }
     handleScroll()
-
     window.addEventListener('scroll', handleScroll)
     return () => {
       window.removeEventListener('scroll', handleScroll)
@@ -34,29 +43,36 @@ export const Header = () => {
   return (
     <>
       <div
-        className={`fixed z-[9] flex flex-col transform ${menuState ? 'scale-y-100' : 'scale-y-0'} origin-top overflow-hidden w-full bg-white transition-transform duration-150 ease-in-out`}
+        className={`fixed z-[8] ${menuState ? 'h-full' : 'h-0'} w-screen transition-colors duration-150 ease-in-out`}
+        style={{ backgroundColor: `rgba(0, 0, 0, ${menuState ? '0.7' : '0'}` }}
       >
-        <span className="h-16 border-b-2 border-mono-500" />
-        <div>
-          <NavButtons redir="" title="Home" setMenuState={setMenuState} />
-          <NavButtons redir="about" title="About" setMenuState={setMenuState} />
-          <NavButtons redir="services" title="Services" setMenuState={setMenuState} />
-          <NavButtons redir="projects" title="Projects" setMenuState={setMenuState} />
-          <NavButtons redir="contact" title="Contact" setMenuState={setMenuState} />
-        </div>
+        <div
+          ref={menuRef}
+          className={`fixed z-[9] flex flex-col transform ${menuState ? 'scale-y-100' : 'scale-y-0'} origin-top overflow-hidden w-full bg-white transition-transform duration-150 ease-in-out`}
+        >
+          <span className="h-16 border-b-2 border-mono-500" />
+          <div>
+            <NavButtons redir="" title="Home" setMenuState={setMenuState} />
+            <NavButtons redir="about" title="About" setMenuState={setMenuState} />
+            <NavButtons redir="services" title="Services" setMenuState={setMenuState} />
+            <NavButtons redir="projects" title="Projects" setMenuState={setMenuState} />
+            <NavButtons redir="contact" title="Contact" setMenuState={setMenuState} />
+          </div>
 
-        <div className={`flex flex-row gap-5 self-center my-2`}>
-          <Socials size={25} base={'black'} hovered={'mono-500'} />
+          <div className={`flex flex-row gap-5 self-center my-2`}>
+            <Socials size={25} base={'black'} hovered={'mono-500'} />
+          </div>
         </div>
       </div>
 
       <div
         className={`fixed z-10 flex flex-row h-16 w-full justify-between items-center md:justify-evenly px-5`}
-        style={{ backgroundColor: `rgba(46, 46, 46, ${bgState}` }}
+        style={{ backgroundColor: `rgba(46, 46, 46, ${menuState ? '0' : bgState}` }}
+        ref={menuRef}
       >
-        <div className={`relative aspect-[174/64] h-12`}>
+        <Link className={`relative aspect-[174/64] h-12`} href="/">
           <Image src={Logo} alt="logo" fill={true} objectFit="contain" />
-        </div>
+        </Link>
         <div
           // className=" md:flex transition-all duration-300 ease-in-out w-0 overflow-hidden md:w-[386.8px] h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
           className="hidden md:flex h-12 gap-5 text-sm tracking-widest align-middle font-robotoSerif"
@@ -77,25 +93,21 @@ export const Header = () => {
           <Socials size={20} base={'white'} hovered={'mono-500'} />
         </div>
 
-        <div
-          className={`${!menuState && 'md:hidden'} flex flex-col h-12 w-12 items-center justify-evenly hover:cursor-pointer`}
-          aria-label="Menu Button"
+        <button
+          className={`${!menuState && 'md:hidden'} flex flex-col h-12 w-12 py-3 px-2 justify-between group hover:cursor-pointer`}
+          aria-label="MenuButton"
+          onClick={() => setMenuState(!menuState)}
         >
-          <button
-            className="flex flex-col justify-between h-6 w-8 group"
-            onClick={() => setMenuState(!menuState)}
-          >
-            <span
-              className={`${menuState ? 'bg-black' : 'bg-white'} group-hover:bg-mono-500 h-0.5 w-8 transition-all ${menuState ? 'rotate-45 translate-y-2.5' : ''}`}
-            />
-            <span
-              className={`bg-white h-0.5 w-8 transition-all group-hover:bg-mono-500 ${menuState ? 'opacity-0' : 'opacity-100'}`}
-            />
-            <span
-              className={`${menuState ? 'bg-black' : 'bg-white'} group-hover:bg-mono-500 h-0.5 w-8 transition-all ${menuState ? '-rotate-45 -translate-y-2.5' : ''}`}
-            />
-          </button>
-        </div>
+          <span
+            className={`${menuState ? 'bg-black' : 'bg-white'} group-hover:bg-mono-500 h-0.5 w-8 transition-all ${menuState ? 'rotate-45 translate-y-2.5' : ''}`}
+          />
+          <span
+            className={`bg-white h-0.5 w-8 transition-all group-hover:bg-mono-500 ${menuState ? 'opacity-0' : 'opacity-100'}`}
+          />
+          <span
+            className={`${menuState ? 'bg-black' : 'bg-white'} group-hover:bg-mono-500 h-0.5 w-8 transition-all ${menuState ? '-rotate-45 -translate-y-2.5' : ''}`}
+          />
+        </button>
       </div>
     </>
   )
