08d0cd1e4c2b1808da0a43b50661b9e536627d3b
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-25 19:45:00 +1100

    Created Stars component with 5 stars and props of size, color and gap

---
+++ src/components/utils/stars.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { FaStar } from 'react-icons/fa'
+
+interface Props {
+    gap: number,
+    size: number,
+    color: string,
+}
+const Stars: React.FC<Props> = (props) => {
+  return (
+    <div className={`flex flex-row gap-${props.gap}`}>
+      <FaStar size={props.size} color={props.color} />
+      <FaStar size={props.size} color={props.color} />
+      <FaStar size={props.size} color={props.color} />
+      <FaStar size={props.size} color={props.color} />
+      <FaStar size={props.size} color={props.color} />
+    </div>
+  )
+}
+
+export default Stars
