0bed863bd64bc30421a565ae8b536238b5a6894f
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-27 10:23:22 +1100

    Made the testimonial card components, still a WIP

---
+++ src/components/testimonials/reviewCard.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Stars from '../utils/stars'
+import Image from 'next/image'
+
+interface Props {
+  name: string
+  profile: string | undefined,
+  review: string,
+}
+
+const ReviewCard = (props: Props) => {
+  return (
+    <div className={`flex flex-col rounded-lg md:w-1/4 px-4 py-4 gap-4 shadow-2xl`}>
+      <div className={`flex flex-row gap-4`}>
+        {props.profile ? (
+          <Image src={props.profile} alt="profilePicture" height={30} width={30} />
+        ) : (
+          // <div className='bg-mono-500 h-8 w-8 justify-center items-center'>
+            <p className={`flex bg-mono-500 text-white h-10 w-10 text-center items-center justify-center rounded-full leading-normal`}>{props.name[0]}</p>
+          // </div>
+        )}
+        <div className='flex flex-col justify-start text-xs gap-1'>
+          <Stars size={12} gap={'[2px]'} color={'#ffd250'} />
+          <h6>{props.name}</h6>
+        </div>
+      </div>
+      <div className='flex bg-mono-200 text-xs md:text-sm rounded-md p-2'>
+        <p>
+          {props.review}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewCard
