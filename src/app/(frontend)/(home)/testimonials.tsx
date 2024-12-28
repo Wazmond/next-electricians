a230a6e2503bafd28c98f55d6c6160c5bd8f684b
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2024-12-28 14:39:13 +1100

    Few changes, nothing major

--- src/app/(frontend)/(home)/testimonials.tsx
+++ src/app/(frontend)/(home)/testimonials.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div className={`flex flex-col justify-center items-center font-robotoSerif py-8`}>
+    <div className={`flex flex-col justify-center items-center font-robotoSerif py-8 bg-mono-300`}>
       <h1>Trusted by many, here's why</h1>
       <div className={`flex flex-row justify-center items-center gap-2`}>
-        <Stars size={12} color={'#ffd250'} gap={'[2px]'}/>
+        <Stars size={12} color={'#ffd250'} gap={'[2px]'} />
         <p className={`text-sm`}>5.0 rating of 9 reviews</p>
       </div>
 
       <div className={`flex flex-row justify-center mt-4 w-full gap-4`}>
-        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined}/>
-        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined}/>
-        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined}/>
+        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined} />
+        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined} />
+        <ReviewCard name="John S." review="lorem asdjajldkajdlka" profile={undefined} />
       </div>
     </div>
   )
