a58c2f19610b9a8829a2cdc258a6b9199ba5c61a
Ryuichi <ryuichi.rooochie@gmail.com>
2024-12-28 16:45:36 +1100

    Apostrophe breaking code. fixed that

--- src/app/(frontend)/(home)/testimonials.tsx
+++ src/app/(frontend)/(home)/testimonials.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div className={`flex flex-col justify-center items-center font-robotoSerif py-8 bg-mono-300`}>
-      <h1>Trusted by many, here's why</h1>
+      <h1>{`Trusted by many, here's why`}</h1>
       <div className={`flex flex-row justify-center items-center gap-2`}>
         <Stars size={12} color={'#ffd250'} gap={'[2px]'} />
         <p className={`text-sm`}>5.0 rating of 9 reviews</p>
