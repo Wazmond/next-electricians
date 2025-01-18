66e538fafedb9831219a409355b57c61b2c2e440
Ryuichi <ryuichi.rooochie@gmail.com>
2025-01-19 06:40:45 +1100

    Changed h1 to h2 due to improper usage

--- src/app/(frontend)/pages/home/landing/landingImage.tsx
+++ src/app/(frontend)/pages/home/landing/landingImage.tsx
@@ -56,7 +56,7 @@ const LandingImage = () => {
       <div className="h-full w-full absolute z-[2] bg-black opacity-70" />
 
       <div className="absolute text-white z-[3] self-center text-center">
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h2>
       </div>
 
       <div className="absolute flex flex-col gap-2 self-end items-center z-[3] mb-4">
