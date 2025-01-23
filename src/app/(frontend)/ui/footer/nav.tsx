65a39dc898fad395d5d10a4d60de2db439381ae4
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-23 11:17:30 +1100

    Made styling changes on the Nav of footer so it doesnt take up the whole screen on mobile. Made it responsive for mobile/larger displays too.

--- src/app/(frontend)/ui/footer/nav.tsx
+++ src/app/(frontend)/ui/footer/nav.tsx
@@ -3,32 +3,38 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className="relative w-full flex flex-row flex-wrap text-mono-500 tracking-wider font-robotoSerif text-center justify-evenly">
-      <Link href="/" className="group w-[calc(33%-1px)]">
-        <h4 className="transition-colors duration-100 ease-in-out group hover:text-mono-50">
-          Home
-        </h4>
-      </Link>
-      <Link href="/about" className="group w-[calc(33%-1px)]">
-        <h4 className="transition-colors duration-100 ease-in-out group hover:text-mono-50">
-          About
-        </h4>
-      </Link>
-      <Link href="/services" className="group w-[calc(33%-1px)]">
-        <h4 className="transition-colors duration-100 ease-in-out group hover:text-mono-50">
-          Services
-        </h4>
-      </Link>
-      <Link href="/projects" className="group w-[calc(49.5%-1.5px)]">
-        <h4 className="transition-colors duration-100 ease-in-out group hover:text-mono-50">
-          Projects
-        </h4>
-      </Link>
-      <Link href="/contact" className="group w-[calc(49.5%-1.5px)]">
-        <h4 className="transition-colors duration-100 ease-in-out group hover:text-mono-50">
-          Contact
-        </h4>
-      </Link>
+    <div className="relative w-full flex flex-col text-mono-500 tracking-wider font-robotoSerif justify-center text-center">
+      <div className="flex flex-row sm:flex-col">
+        <Link href="/" className="flex-1">
+          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Home</h4>
+        </Link>
+        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
+        <Link href="/about" className="flex-1">
+          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">About</h4>
+        </Link>
+        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
+        <Link href="/services" className="flex-1">
+          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
+            Services
+          </h4>
+        </Link>
+      </div>
+
+      <div className="flex flex-row sm:flex-col">
+        <span className="flex-1" />
+        <Link href="/projects" className="flex-[2]">
+          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
+            Projects
+          </h4>
+        </Link>
+        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
+        <Link href="/contact" className="flex-[2]">
+          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
+            Contact
+          </h4>
+        </Link>
+        <span className="flex-1" />
+      </div>
     </div>
   )
 }
