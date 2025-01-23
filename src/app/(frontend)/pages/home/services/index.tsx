795a6661daa830fda0aeab482f90f6d24da42d53
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-23 13:22:17 +1100

    Created and styled card component for services and added icons for each wrapped in a div in grid styling, added padding and spacing on parent.

--- src/app/(frontend)/pages/home/services/index.tsx
+++ src/app/(frontend)/pages/home/services/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
+import { SlWrench } from 'react-icons/sl'
+import { VscRobot } from 'react-icons/vsc'
+import { HiOutlineLightBulb } from 'react-icons/hi'
+import { RxLightningBolt, RxMagnifyingGlass } from 'react-icons/rx'
+import { RiShieldFlashLine } from 'react-icons/ri'
+import ServicesCard from './servicesCard'
+
 const HomeServices = () => {
   return (
-    <div className="w-full h-auto flex relative">
-      <h2>Our Services</h2>
-      <div>
-        <p>Maintenance</p>
-        <p>Home Automation</p>
-        <p>Power, Lights, & Cabling</p>
-        <p>Energy Saving Solutions</p>
-        <p>Fault Find & Rectification</p>
-        <p>Switchboards</p>
+    <div className="w-full h-auto bg-sky-600 flex relative justify-center text-white py-8 px-8">
+      <div className="w-full max-w-[1178px] flex flex-col text-center gap-4">
+        <h2>Our Services</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 rounded-lg overflow-clip">
+          <ServicesCard title="Maintenance" icon={SlWrench} />
+          <ServicesCard title="Home Automation" icon={VscRobot} />
+          <ServicesCard title="Power, Lights & Cabling" icon={HiOutlineLightBulb} />
+          <ServicesCard title="Energy Saving Solutions" icon={RxLightningBolt} />
+          <ServicesCard title="Fault Find & Rectification" icon={RxMagnifyingGlass} />
+          <ServicesCard title="Surge Protection" icon={RiShieldFlashLine} />
+        </div>
       </div>
     </div>
   )
--- src/app/(frontend)/pages/home/services/servicesCard.tsx
+++ src/app/(frontend)/pages/home/services/servicesCard.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 
+import { IconType } from 'react-icons'
+
 interface Props {
   title: string
+  icon: IconType
 }
 
 const ServicesCard = (props: Props) => {
-  return  (
-  <div className="">
-    {/* An icon for the services? possibly... */}
-    <h3>{props.title}</h3>
-    {/* A description required? Will have to ask */}
-  </div>
-)
+  return (
+    <div className="bg-white flex flex-col items-center py-4">
+      <div className="border border-sky-600 rounded-full p-2 mb-4">
+        <props.icon size={30} className="text-sky-600" />
+      </div>
+      <p className="text-black">{props.title}</p>
+      {/* A description required? Will have to ask */}
+    </div>
+  )
 }
 
 export default ServicesCard
