d68ed6de29b7bf8d74142634bfe011c20a85c715
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-23 11:47:32 +1100

    Changed color of text, and placement of each components to make it look nicer, cleaner and uniform.

--- src/app/(frontend)/ui/footer/index.tsx
+++ src/app/(frontend)/ui/footer/index.tsx
@@ -4,27 +4,29 @@ import Socials from '@/components/socials'
 
 const Footer = () => {
   return (
-    <div className="flex flex-col sm:flex-row items-center px-12 gap-3 bg-mono-900 py-3 font-robotoSerif text-white">
+    <div className="bg-mono-900">
       {/* <Image src={Logo} alt="logo" objectFit="contain" /> */}
 
       {/* <h3 className="text-mono-400 tracking-widest font-robotoSerif">NextElectricians Pty Ltd</h3> */}
-      <span className="flex flex-1 " />
-      <div className="flex flex-1 flex-col w-full">
-        <h3>Explore</h3>
-        <Nav />
-      </div>
+      {/* <span className="flex flex-1" /> */}
+      <div className="h-full w-full max-sm:max-w-[450px] max-w-[1178px] place-self-center flex flex-col sm:flex-row py-3 px-12 gap-3 font-robotoSerif text-white">
+        <div className="flex flex-1 flex-col w-full text-center text-mono-400">
+          <h3 className="underline underline-offset-4 mb-1 text-white">Explore</h3>
+          <Nav />
+        </div>
 
-      <div className='flex-col text-center'>
-        <h3 className='underline'>Contact Us</h3>
-        <p>ABN: 64 682 401 510</p>
-        <p>Hugh@NextElectricians.com.au</p>
-        <p>Ph: 0435 367 571</p>
-      </div>
+        <div className="flex-col text-center text-mono-400">
+          <h3 className="text-white underline underline-offset-4 mb-1">Contact Us</h3>
+          <p>ABN: 64 682 401 510</p>
+          <p>Hugh@NextElectricians.com.au</p>
+          <p>Ph: 0435 367 571</p>
+        </div>
 
-      <div className="flex flex-1 flex-col">
-        <h3 className="underline">Our Socials</h3>
-        <div className="flex flex-row gap-5 items-center">
-          <Socials size={20} base="text-mono-500" hovered="hover:text-mono-50" />
+        <div className="flex flex-1 flex-col text-center items-center">
+          <h3 className="underline underline-offset-4 mb-2">Our Socials</h3>
+          <div className="flex flex-row gap-5 items-center">
+            <Socials size={20} base="text-mono-400" hovered="hover:text-mono-50" />
+          </div>
         </div>
       </div>
     </div>
--- src/app/(frontend)/ui/footer/nav.tsx
+++ src/app/(frontend)/ui/footer/nav.tsx
@@ -3,35 +3,29 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className="relative w-full flex flex-col text-mono-500 tracking-wider font-robotoSerif justify-center text-center">
-      <div className="flex flex-row sm:flex-col">
-        <Link href="/" className="flex-1">
-          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Home</h4>
+    <div className="relative w-auto flex flex-col tracking-wider font-robotoSerif justify-center text-center">
+      <div className="flex flex-row flex-wrap sm:flex-col">
+        <Link href="/" className="flex-1 px-2">
+          <p className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Home</p>
         </Link>
-        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
-        <Link href="/about" className="flex-1">
-          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">About</h4>
+        <span className="max-[245px]:hidden sm:hidden h-[8px] w-[8px] rounded-full bg-mono-400 self-center" />
+        <Link href="/about" className="flex-1 px-2">
+          <p className="transition-colors duration-100 ease-in-out  hover:text-mono-50">About</p>
         </Link>
-        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
-        <Link href="/services" className="flex-1">
-          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
-            Services
-          </h4>
+        <span className="max-[245px]:hidden sm:hidden h-[8px] w-[8px] rounded-full bg-mono-400 self-center" />
+        <Link href="/services" className="flex-1 px-2">
+          <p className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Services</p>
         </Link>
       </div>
 
-      <div className="flex flex-row sm:flex-col">
+      <div className="flex flex-row flex-wrap sm:flex-col">
         <span className="flex-1" />
-        <Link href="/projects" className="flex-[2]">
-          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
-            Projects
-          </h4>
+        <Link href="/projects" className="flex-[2] px-2">
+          <p className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Projects</p>
         </Link>
-        <span className="sm:hidden h-[8px] w-[8px] rounded-full bg-mono-500 self-center" />
-        <Link href="/contact" className="flex-[2]">
-          <h4 className="transition-colors duration-100 ease-in-out  hover:text-mono-50">
-            Contact
-          </h4>
+        <span className="max-[245px]:hidden sm:hidden h-[8px] w-[8px] rounded-full bg-mono-400 self-center" />
+        <Link href="/contact" className="flex-[2] px-2">
+          <p className="transition-colors duration-100 ease-in-out  hover:text-mono-50">Contact</p>
         </Link>
         <span className="flex-1" />
       </div>
