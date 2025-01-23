352987c13cc860ae9b35527bc360d6d520b20cca
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-23 13:24:59 +1100

    Experimenting with a bg change on testimonials so its blue. Logo is blue so figured blue would be nice. Not sure how I feel, but I like how its not just mono on the background.

--- src/app/(frontend)/pages/home/testimonials/index.tsx
+++ src/app/(frontend)/pages/home/testimonials/index.tsx
@@ -6,13 +6,13 @@ import React from 'react'
 const Testimonials = () => {
   return (
     <div
-      className={`flex flex-col justify-center items-center font-robotoSerif py-8 bg-mono-200 w-full gap-4`}
+      className={`flex flex-col justify-center items-center font-robotoSerif py-8 bg-sky-600 w-full gap-4`}
     >
       <div>
-        <h2>{`Trusted by many, here's why`}</h2>
+        <h2 className="text-white">{`Trusted by many, here's why`}</h2>
         <div className={`flex flex-row justify-center items-center gap-2`}>
           <Stars size={12} color={'#ffd250'} gap={'[2px]'} />
-          <p className={`text-sm`}>5.0 rating of 9 reviews</p>
+          <p className={`text-sm text-white`}>5.0 rating of 9 reviews</p>
         </div>
       </div>
 
