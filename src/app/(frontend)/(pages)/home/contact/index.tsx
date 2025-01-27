7503d0ec76741ff0870ae3a0a5eca120bbfeef66
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-01-27 03:47:01 +0000

    Cleaned up code by removing the useForm hook and not passing props through component.

--- src/app/(frontend)/(pages)/home/contact/index.tsx
+++ src/app/(frontend)/(pages)/home/contact/index.tsx
@@ -1,21 +1,11 @@
-'use client'
-
 import ContactForm from '@/components/contactForm'
-import { useForm } from '@formspree/react'
 import React from 'react'
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm('xkggpblq')
-
   return (
     <div className="self-center w-full max-w-[1178px] px-8 font-robotoSerif flex flex-col items-center gap-4 py-8">
       <h2 className="text-center">Get in touch with NextElectricians</h2>
-
-      {state.succeeded ? (
-        <p className="text-center">{`Thank's for your enquiry, a NextElectrician will be in touch with you shortly.`}</p>
-      ) : (
-        <ContactForm state={state} handleSubmit={handleSubmit}/>
-      )}
+      <ContactForm />
     </div>
   )
 }
