a346fa26e0387a980a089f179c1dce654c1c6612
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-01-27 01:21:42 +0000

    started the About Page

--- src/app/(frontend)/(pages)/about/page.tsx
+++ src/app/(frontend)/(pages)/about/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>About Page</div>
+    <div className='w-full flex flex-1 flex-col'>
+      <span className='fixed w-full h-16 bg-[rgba(46,46,46)]'/> 
+      <span className='w-full h-16'/>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat sapiente, aliquid corporis inventore iusto harum obcaecati quo?</p>
+
+    </div>
   )
 }
 
