0ebb5dbfbdc8e47072942e9afa0f3f2195c0dc52
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-28 12:56:07 +1100

    Removed the link/redir of projects card

--- src/components/projectsComponent/projectCard.tsx
+++ src/components/projectsComponent/projectCard.tsx
@@ -11,17 +11,17 @@ const ProjectCard = (props: Props) => {
     <div
       className={`aspect-[4/3] ${props.data.image ? 'flex' : 'hidden'} flex-col rounded-lg shadow-lg relative overflow-hidden`}
     >
-      <Link href="/projects/">
-        <div className="">
-          <Image src={props.data.image} alt="Project Photo" fill={true} className="object-cover" />
-        </div>
+      {/* <Link href="/projects/"> */}
+      <div className="">
+        <Image src={props.data.image} alt="Project Photo" fill={true} className="object-cover" />
+      </div>
 
-        <div className="hidden absolute sm:flex hover:opacity-100 opacity-0 transform transition-opacity duration-150 ease-in-out h-full w-full z-[5] bg-mono-200 bg-opacity-90 py-8 px-8 text-center justify-between flex-col">
-          <h3>{props.data.title}</h3>
-          <p>{props.data.desc}</p>
-          <p>{props.data.date}</p>
-        </div>
-      </Link>
+      <div className="hidden absolute sm:flex hover:opacity-100 opacity-0 transform transition-opacity duration-150 ease-in-out h-full w-full z-[5] bg-mono-200 bg-opacity-90 py-8 px-8 text-center justify-between flex-col">
+        <h3>{props.data.title}</h3>
+        <p>{props.data.desc}</p>
+        <p>{props.data.date}</p>
+      </div>
+      {/* </Link> */}
     </div>
   )
 }
