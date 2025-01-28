41d3e6f72d4883d576308d22b0ac5520c7d03640
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-01-28 12:46:33 +1100

    Separated the projects component to its own components folder and updated importing of component

--- src/app/(frontend)/(pages)/home/projects/index.tsx
+++ src/app/(frontend)/(pages)/home/projects/index.tsx
@@ -1,8 +1,6 @@
-'use client'
-import ProjectCard from '@/components/projectCard'
+import ProjectsComponent from '@/components/projectsComponent'
 import Link from 'next/link'
 import React from 'react'
-import projectsData from 'src/collections/projectsData.json'
 
 const Projects = () => {
   //This segment is to be replaced when PayloadCMS is up and running, or atleast when I learn how to use it hahaha.
@@ -10,12 +8,7 @@ const Projects = () => {
   return (
     <div className="gap-6 py-8 flex flex-col self-center w-full items-center font-robotoSerif max-w-[1178px] px-8">
       <h2>Our Previous Projects</h2>
-      <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4">
-        {projectsData.map((data: ProjectsDataType, key: number) => (
-          <ProjectCard key={key} data={data} />
-        ))}
-      </div>
-
+      <ProjectsComponent />
       <Link className="w-auto bg-white drop-shadow-lg py-2 px-4 rounded-lg" href="/projects">
         View More
       </Link>
--- src/app/(frontend)/(pages)/projects/page.tsx
+++ src/app/(frontend)/(pages)/projects/page.tsx
@@ -1,13 +1,20 @@
+import ProjectsComponent from '@/components/projectsComponent'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div className='flex flex-col flex-1'>
-      <span className='fixed w-full h-16 bg-[rgba(46,46,46)]'/> 
-      <span className='w-full h-16'/>
-      <h1>Projects</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat sapiente, aliquid corporis inventore iusto harum obcaecati quo?</p>
-
+    <div className="flex flex-col flex-1">
+      <span className="fixed w-full h-16 bg-[rgba(46,46,46)]" />
+      <span className="w-full h-16" />
+      <div className="w-full max-w-[1178px] self-center px-8 py-8 flex flex-col gap-4 text-center">
+        <h1>Projects</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam
+          impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat
+          sapiente, aliquid corporis inventore iusto harum obcaecati quo?
+        </p>
+        <ProjectsComponent />
+      </div>
     </div>
   )
 }
