e7079f96345250179afef6c9c24f8c9dfc90d999
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-01-30 03:36:07 +0000

    Centering button and introducing smooth scroll behaviour

--- src/app/(frontend)/(pages)/home/services/index.tsx
+++ src/app/(frontend)/(pages)/home/services/index.tsx
@@ -11,9 +11,9 @@ import CTAButton from '@/components/ctaButton'
 const HomeServices = () => {
   return (
     <div className="w-full h-auto bg-sky-600 flex relative justify-center text-white py-8 px-8">
-      <div className="w-full max-w-[1178px] flex flex-col text-center gap-4">
+      <div className="w-full max-w-[1178px] flex flex-col items-center gap-4">
         <h2>Our Services</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 rounded-lg overflow-clip">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-2 rounded-lg">
           <ServicesCard title="Maintenance" icon={SlWrench} />
           <ServicesCard title="Home Automation" icon={VscRobot} />
           <ServicesCard title="Power, Lights & Cabling" icon={HiOutlineLightBulb} />
