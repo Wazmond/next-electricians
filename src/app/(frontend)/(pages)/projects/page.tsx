0908d37af3a420c93ac0c0d449c0305c2e37399b
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-01-30 03:40:04 +0000

    Introduced padding and max width on the other pages

--- src/app/(frontend)/(pages)/projects/page.tsx
+++ src/app/(frontend)/(pages)/projects/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
     <div className="flex flex-col flex-1">
       <span className="fixed w-full h-16 bg-[rgba(46,46,46)]" />
       <span className="w-full h-16" />
-      <div className="w-full max-w-[1178px] self-center px-8 py-8 flex flex-col gap-4 text-center">
+      <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
         <h1>Projects</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam
--- src/app/(frontend)/(pages)/services/page.tsx
+++ src/app/(frontend)/(pages)/services/page.tsx
@@ -5,8 +5,11 @@ const Page = () => {
     <div className='flex flex-col flex-1'>
       <span className='fixed w-full h-16 bg-[rgba(46,46,46)]'/> 
       <span className='w-full h-16'/>
-      <h1>Our Services</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat sapiente, aliquid corporis inventore iusto harum obcaecati quo?</p>
+      <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
+        <h1>Our Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat sapiente, aliquid corporis inventore iusto harum obcaecati quo?</p>        
+      </div>
+
 
     </div>
   )
