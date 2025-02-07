68ba44b33dffe5653b7bc1cfb98ed4782f3e41ea
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-02-07 21:50:42 +1100

    key changed... will need to get CMS up and running

--- src/components/contactForm/index.tsx
+++ src/components/contactForm/index.tsx
@@ -3,55 +3,51 @@ import React from 'react'
 import InputField from './inputFields'
 import { useForm } from '@formspree/react'
 
-
-
 const ContactForm = () => {
-  const [state, handleSubmit] = useForm('xkggpblq')
+  const [state, handleSubmit] = useForm('xvgzqrry')
 
   return (
     <>
-          {state.succeeded ? (
-            <p className="text-center">{`Thank's for your enquiry, a NextElectrician will be in touch with you shortly.`}</p>
-          ) : (
-            <form
-            onSubmit={handleSubmit}
-            method="post"
-            className="flex flex-col gap-4 items-center w-full max-w-lg"
+      {state.succeeded ? (
+        <p className="text-center">{`Thank's for your enquiry, a NextElectrician will be in touch with you shortly.`}</p>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          method="post"
+          className="flex flex-col gap-4 items-center w-full max-w-lg"
+        >
+          <InputField state name="name" title="Name" type="text" placeholder="e.g John Smith" />
+          <InputField
+            state
+            name="phone"
+            title="Phone Number"
+            type="tel"
+            placeholder="e.g 0412 345 678"
+          />
+          <InputField
+            state
+            name="email"
+            title="Email Address"
+            type="email"
+            placeholder="e.g email.address@example.com"
+          />
+          <InputField
+            state
+            name="desc"
+            title="Description"
+            type="text"
+            placeholder="e.g I want my home to look stunning, please do some lighting magic!"
+          />
+          <button
+            type="submit"
+            disabled={state.submitting}
+            className="w-auto bg-blue-400 text-white py-2 px-4 rounded-lg"
           >
-            <InputField state name="name" title="Name" type="text" placeholder="e.g John Smith" />
-            <InputField
-              state
-              name="phone"
-              title="Phone Number"
-              type="tel"
-              placeholder="e.g 0412 345 678"
-            />
-            <InputField
-              state
-              name="email"
-              title="Email Address"
-              type="email"
-              placeholder="e.g email.address@example.com"
-            />
-            <InputField
-              state
-              name="desc"
-              title="Description"
-              type="text"
-              placeholder="e.g I want my home to look stunning, please do some lighting magic!"
-            />
-            <button
-              type="submit"
-              disabled={state.submitting}
-              className="w-auto bg-blue-400 text-white py-2 px-4 rounded-lg"
-            >
-              {state.submitting ? 'Submitting...' : 'Get a callback'}
-            </button>
-          </form>
-          )}
-
+            {state.submitting ? 'Submitting...' : 'Get a callback'}
+          </button>
+        </form>
+      )}
     </>
-
   )
 }
 
