b02c6e8aa42bceec72be6aef2f82b5c5e4d9149e
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-02-18 18:58:39 +0000

    Replaced placeholder text with text provided by Hugh

--- src/app/(frontend)/(pages)/projects/page.tsx
+++ src/app/(frontend)/(pages)/projects/page.tsx
@@ -8,10 +8,8 @@ const Page = () => {
       <span className="w-full h-16" />
       <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
         <h1>Projects</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus nesciunt veniam
-          impedit tempore expedita possimus non corrupti, nihil sequi fuga voluptatem repellat
-          sapiente, aliquid corporis inventore iusto harum obcaecati quo?
+        <p className='text-center'>
+        At Next Electricians, we take pride in our work. Every project is completed with attention to detail and care, treating your home as if it were our own. No matter the size of the job, we ensure professionalism, respect, and high-quality work from start to finish. See some of our recent protects below:
         </p>
         <ProjectsComponent />
       </div>
