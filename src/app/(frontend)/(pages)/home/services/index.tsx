c15a8b2d010faeb926835ee5d614a075e84d501f
Ryuichi Nakamura <147779932+Wazmond@users.noreply.github.com>
2025-02-18 18:53:03 +0000

    Got rid of a service, added text for services section

--- src/app/(frontend)/(pages)/home/services/index.tsx
+++ src/app/(frontend)/(pages)/home/services/index.tsx
@@ -13,14 +13,14 @@ const HomeServices = () => {
     <>
       <div className="w-full max-w-[1178px] flex flex-col items-center gap-4">
         <h2 className="font-robotoSerif">Our Services</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quas eveniet iusto impedit harum architecto optio tempora. Voluptatum praesentium asperiores, sapiente, culpa repellat mollitia velit cupiditate quae dolorum, eum amet.</p>
+        <p className='text-center'>NextElectricians provides services for both residential and commercial sites. Services we provide are but not limited to:</p>
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-2 rounded-lg">
           <ServicesCard title="Maintenance" icon={SlWrench} />
           <ServicesCard title="Intelligent Lighting Solutions" icon={VscRobot} />
           <ServicesCard title="Power, Lights & Cabling" icon={HiOutlineLightBulb} />
           <ServicesCard title="Energy Saving Solutions" icon={RxLightningBolt} />
           <ServicesCard title="Fault Find & Rectification" icon={RxMagnifyingGlass} />
-          <ServicesCard title="Data Cabling" icon={RiShieldFlashLine} />
+          {/* <ServicesCard title="Data Cabling" icon={RiShieldFlashLine} /> */}
           <ServicesCard title="CCTV Installations" icon={RiShieldFlashLine} />
         </div>
         <CTAButton href="#contact" className="bg-blue-500 hover:bg-blue-300 text-white" text="Get a Quote"/>
