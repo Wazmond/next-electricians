db4c0f06b3d7677c85746f89874dd1df022244de
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-04-09 15:11:28 +1000

    Updated ProjectsPage to use content via payload

--- src/app/(frontend)/(pages)/projects/page.tsx
+++ src/app/(frontend)/(pages)/projects/page.tsx
@@ -1,20 +1,17 @@
 import HeaderBg from '@/components/HeaderBg'
 import ProjectsComponent from '@/components/projectsComponent'
+import { payload } from '@/hooks/payload'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const pageContent = await payload.findGlobal({ slug: 'projectsPage' })
   return (
     <div className="flex flex-col flex-1">
       <HeaderBg />
       <span className="w-full h-16" />
       <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
-        <h1>Our Work</h1>
-        <p className="text-center">
-          At Next Electricians, we take pride in our work. Every project is completed with attention
-          to detail and care, treating your home as if it were our own. No matter the size of the
-          job, we ensure professionalism, respect, and high-quality work from start to finish. See
-          some of our recent protects below:
-        </p>
+        <h1>{pageContent?.title}</h1>
+        <p className="text-center">{pageContent?.text}</p>
         <ProjectsComponent />
       </div>
     </div>
