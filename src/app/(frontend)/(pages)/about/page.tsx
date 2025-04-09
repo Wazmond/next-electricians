b488075008823ec201052888d38df6956be173fc
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-04-09 15:11:37 +1000

    Updated AboutPage to use content via payload

--- src/app/(frontend)/(pages)/about/page.tsx
+++ src/app/(frontend)/(pages)/about/page.tsx
@@ -1,18 +1,16 @@
 import HeaderBg from '@/components/HeaderBg'
+import { payload } from '@/hooks/payload'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+  const pageContent = await payload.findGlobal({ slug: 'aboutPage' })
   return (
     <div className="flex flex-col flex-1">
       <HeaderBg />
       <span className="w-full h-16" />
       <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
-        <h1>About Us</h1>
-        <p className="text-wrap">
-          {`We’re Lachlan and Hugh - licensed electricians with over five years of experience working with commercial projects, including solar and data installations. 
-            Now, we’re bringing our expertise to homes and businesses, helping you create spaces with professional lighting, cabling, and electrical solutions. 
-            Our goal is to provide high-quality electrical services that enhance your space while ensuring safety and efficiency. We take pride in every project we complete. `}
-        </p>
+        <h1>{pageContent.title}</h1>
+        <p className="text-wrap">{pageContent.text}</p>
       </div>
     </div>
   )
