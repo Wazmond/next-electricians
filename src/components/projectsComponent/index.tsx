0c7fc1ceed7ad99bc7d5ad025e2e5f7ac259c4c3
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-04-18 20:38:47 +1000

    formatting

--- src/components/projectsComponent/index.tsx
+++ src/components/projectsComponent/index.tsx
@@ -11,34 +11,30 @@ const ProjectsComponent = async () => {
   const cached = unstable_cache(
     async () => {
       const { docs } = await payload.find({
-        collection: "projects"
+        collection: 'projects',
       })
       return docs
-    }, 
-    [], 
+    },
+    [],
     {
-      revalidate: 60
-    }
-  ) 
+      revalidate: 60,
+    },
+  )
 
   const projects = await cached()
-  
+
   return (
     <>
-    {projects ? (    
-      <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4">
-        {projects.map((data: Project) => (
-          <ProjectCard key={data.id} data={data} />
-        ))}
-      </div>
-    ) : (
-      <p className='text-mono-500'>
-        Loading projects...
-      </p>
-    )}
-
+      {projects ? (
+        <div className="w-full grid grid-cols-2 md:grid-cols-3 gap-4">
+          {projects.map((data: Project) => (
+            <ProjectCard key={data.id} data={data} />
+          ))}
+        </div>
+      ) : (
+        <p className="text-mono-500">Loading projects...</p>
+      )}
     </>
-
   )
 }
 
