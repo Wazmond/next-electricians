96f96b746cb6e3b75f745a81ce03c2bb62cba1df
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-04-22 10:35:44 +1000

    Fixed build error due to params not being typed as a Promise

--- src/app/(frontend)/(pages)/projects/[id]/page.tsx
+++ src/app/(frontend)/(pages)/projects/[id]/page.tsx
@@ -2,13 +2,9 @@ import HeaderBg from '@/components/HeaderBg'
 import ProjectDetails from '@/components/projectDetails'
 import React from 'react'
 
-interface Props {
-  params: {
-    id: string
-  }
-}
+type Props = Promise<{ id: string }>
 
-const page = async ({ params }: Props) => {
+const page = async ({ params }: { params: Props }) => {
   const { id } = await params
   return (
     <>
--- src/app/(frontend)/@projectModal/projects/(.)[id]/page.tsx
+++ src/app/(frontend)/@projectModal/projects/(.)[id]/page.tsx
@@ -1,13 +1,9 @@
 import ProjectDetails from '@/components/projectDetails'
 import React from 'react'
 
-interface Props {
-  params: {
-    id: string
-  }
-}
+type Props = Promise<{ id: string }>
 
-const page = async ({ params }: Props) => {
+const page = async ({ params }: { params: Props }) => {
   const { id } = await params
   return (
     <div className="fixed h-full w-full flex bg-black/50 items-center justify-center p-8">
