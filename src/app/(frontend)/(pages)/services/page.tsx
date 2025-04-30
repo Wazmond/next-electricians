1efc25da60e0ef0e34b2d94ceef0c294146a13a1
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-05-01 05:55:33 +1000

    Put code from serviceComponent into the page.tsx, to see if that does anything

--- src/app/(frontend)/(pages)/services/page.tsx
+++ src/app/(frontend)/(pages)/services/page.tsx
@@ -1,9 +1,12 @@
 import EnquiryForm from '@/components/enquiryForm'
 import HeaderBg from '@/components/HeaderBg'
 import ServicesComponent from '@/components/servicesComponent'
+import MiddleColumn from '@/components/servicesComponent/midCol'
+import ServicesCard from '@/components/servicesComponent/servicesCard'
 // import ServicesCard from '@/components/servicesCard'
 import { payload } from '@/hooks/payload'
-import { Service } from '@/payload-types'
+import { Media, Service } from '@/payload-types'
+import Image from 'next/image'
 import React, { Suspense } from 'react'
 
 type Props = {
@@ -13,6 +16,12 @@ const Page = async ({ searchParams }: Props) => {
   const pageContent = await payload.findGlobal({
     slug: 'servicesPage',
   })
+
+  const { docs } = await payload.find({
+    collection: 'services',
+  })
+
+  const param: number = (await searchParams).service ?? 1
 
   return (
     <div className="flex flex-col flex-1">
@@ -24,11 +33,49 @@ const Page = async ({ searchParams }: Props) => {
         </div>
         <EnquiryForm text={pageContent.enquiryTitle} buttonText="Enquire Now" />
 
-        {/* <Suspense fallback={<></>}> */}
-        <section className="bg-light-blue w-full items-center flex justify-center py-8">
-          <ServicesComponent searchParams={searchParams} />
-        </section>
-        {/* </Suspense> */}
+        <Suspense fallback={<></>}>
+          <section className="bg-light-blue w-full items-center flex justify-center py-8">
+            <div className="max-w-[1178px] w-full self-center flex flex-row">
+              {/* Services column with all services */}
+              <div className="relative flex flex-1 flex-row h-min">
+                <div className=" flex flex-col w-full gap-2">
+                  {docs.map((service, index) => (
+                    <ServicesCard
+                      service={service}
+                      index={index + 1}
+                      param={param}
+                      key={service.id}
+                    />
+                  ))}
+                </div>
+                <MiddleColumn param={param} docs={docs} />
+              </div>
+
+              <div
+                style={{
+                  borderTopLeftRadius: `${param == 1 ? '0' : '12px'}`,
+                  borderBottomLeftRadius: `${param == docs.length ? '0' : '12px'}`,
+                }}
+                className="flex flex-1 flex-col items-center bg-white rounded-tr-xl rounded-br-xl rounded-bl-lg p-4 gap-4 z-[2]"
+              >
+                <Suspense fallback={<></>}>
+                  <div className="relative w-full h-[400px] aspect-auto flex items-center justify-center">
+                    <Image
+                      src={(docs[param - 1].image as Media).url!}
+                      alt={(docs[param - 1].image as Media).alt}
+                      height={(docs[param - 1].image as Media).height as number}
+                      width={(docs[param - 1].image as Media).width as number}
+                      className="object-contain max-w-full max-h-full"
+                    />
+                  </div>
+
+                  <h3>{docs[param - 1].title}</h3>
+                  <p>{docs[param - 1].description}</p>
+                </Suspense>
+              </div>
+            </div>
+          </section>
+        </Suspense>
       </div>
     </div>
   )
