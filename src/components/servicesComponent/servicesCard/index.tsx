a825c3893327f06b9e9e8bac40ae43a9a4b67a08
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-05-01 04:40:57 +1000

    Removed image from component, added styling changes.

--- src/components/servicesComponent/servicesCard/index.tsx
+++ src/components/servicesComponent/servicesCard/index.tsx
@@ -5,29 +5,18 @@ import React from 'react'
 
 interface Props {
   service: Service
-  id: number
+  param: number
   index: number
 }
 
-const ServicesCard = ({ service, id, index }: Props) => {
+const ServicesCard = ({ service, param, index }: Props) => {
   return (
-    // Change width of card Padding or Margin to fill gap between card and details component
     <Link href={`/services?service=${index}`} scroll={false}>
       <div
-        className={`${service.image ? 'flex' : 'hidden'} flex-row rounded-lg relative h-auto w-full p-4 gap-2 justify-between items-start bg-white border-2 border-white hover:cursor-pointer hover:border-next-blue`}
+        style={{ borderRadius: `12px ${param == index ? '0 0' : '12px 12px'} 12px` }}
+        className={`${service.image ? 'flex' : 'hidden'} flex-row relative h-16 w-full p-4 gap-2 justify-between items-start bg-white border-2 border-white hover:cursor-pointer hover:border-next-blue`}
       >
-        <div className="flex flex-col text-left font-robotoMono text-clip">
-          <h3 className="text-blue-900">{service.title}</h3>
-        </div>
-        <div className="relative h-[84px] aspect-square">
-          <Image
-            src={(service.image as Media).url!}
-            alt={(service.image as Media).alt!}
-            height={(service.image as Media).height!}
-            width={(service.image as Media).width!}
-            className="object-cover"
-          />
-        </div>
+        <h3 className="text-blue-900 font-robotoMono">{service.title}</h3>
       </div>
     </Link>
   )
