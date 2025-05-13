72d8a4ffb3ff6048153c1b532a72047bfc601850
Ryuichi Nakamura <Ryuichi.rooochie@gmail.com>
2025-05-13 12:07:39 +1000

    Added padding

--- src/app/(frontend)/(pages)/services/servicesSection/index.tsx
+++ src/app/(frontend)/(pages)/services/servicesSection/index.tsx
@@ -6,7 +6,7 @@ const ServicesSection = async () => {
   const { docs } = await payload.find({ collection: 'services' })
 
   return (
-    <div className="max-w-[1178px] w-full self-center flex flex-row">
+    <div className="max-w-[1178px] w-full self-center flex flex-row p-4">
       <SearchParamClient docs={docs} />
     </div>
   )
