f8342577e661e8e596c8e7210af17d6212bce3fe
Ryuichi Nakamura <rynak25@gmail.com>
2025-09-22 18:27:37 +1000

    Push

--- src/app/(frontend)/(pages)/projects/page.tsx
+++ src/app/(frontend)/(pages)/projects/page.tsx
@@ -13,7 +13,7 @@ const Page = async () => {
       <div className="w-full max-w-[1178px] self-center p-8 flex flex-col gap-4 text-center">
         <h1>{pageContent?.title}</h1>
         <p className="text-center">{pageContent?.text}</p>
-        <p className="text-center">Have a project in mind? Big or small, we're ready to help bring it to life.</p>
+        <p className="text-center">{"Have a project in mind? Big or small, we're ready to help bring it to life."}</p>
         <p className="text-center"><Link href={"/contact"} className={'text-blue-800'}>Contact us</Link> to get started</p>
         <ProjectsComponent />
       </div>
