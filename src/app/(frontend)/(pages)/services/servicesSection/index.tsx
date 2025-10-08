99dda0e82eb0d760c5f738ab70a9784133f67c65
Ryuichi Nakamura <rynak25@gmail.com>
2025-10-08 14:36:36 +1100

    conditional on services

--- src/app/(frontend)/(pages)/services/servicesSection/index.tsx
+++ src/app/(frontend)/(pages)/services/servicesSection/index.tsx
@@ -6,9 +6,13 @@ const ServicesSection = async () => {
   const { docs } = await payload.find({ collection: 'services' })
 
   return (
-    <div className="max-w-[1178px] w-full self-center flex flex-row p-4">
-      <SearchParamClient docs={docs} />
-    </div>
+    <>
+      {docs.length > 0 && (
+        <div className="max-w-[1178px] w-full self-center flex flex-row p-4">
+          <SearchParamClient docs={docs} />
+        </div>
+      )}
+    </>
   )
 }
 
